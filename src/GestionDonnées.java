41ad35dfd3db526e24a1679dbf7d856b23a7423c
gpican <gaetan.pican@gmail.com>
2017-05-03 13:56:05 +0200

    Update GestionDonnées.java

--- src/GestionDonnées.java
+++ src/GestionDonnées.java
@@ -1,4 +1,4 @@
-import java.net.URL;
+iimport java.net.URL;
 import java.net.URLEncoder;
 import java.util.Scanner;
 
@@ -10,8 +10,8 @@ public class GestionDonnees {
 	String jsonToGoogle;
 	static double[] coordLat = new double[5];
 	static double[] coordLng = new double[5];
-	static double centerLat;
-	static double centerLng;
+	static double centerLat = 48.85340329999999;
+	static double centerLng = 2.3487835999999334;
 	JSONObject bars;
 	JSONObject restos;
 	JSONObject boites;
@@ -58,40 +58,40 @@ public class GestionDonnees {
 	}
 	
 	public static void getCenterCoord(){
-		double bounds = new google.maps.LatLngBounds();
-		var i;
-
-		// The Bermuda Triangle
-		var polygonCoords = [
-		  new google.maps.LatLng(25.774252, -80.190262),
-		  new google.maps.LatLng(18.466465, -66.118292),
-		  new google.maps.LatLng(32.321384, -64.757370),
-		  new google.maps.LatLng(25.774252, -80.190262)
-		];
-
-		for (i = 0; i < polygonCoords.length; i++) {
-		  bounds.extend(polygonCoords[i]);
+		int sommeLat = 0;
+		int sommeLng = 0;
+		int countLat = 0;
+		int countLng = 0;
+		
+		for (int i=0; i<5; i++){
+			if (coordLat[i] != 0 || coordLng[i] != 0){
+				sommeLat += coordLat[i];
+				countLat ++;
+				sommeLng += coordLng[i];
+				countLng ++;
+			} 
 		}
-
-		// The Center of the Bermuda Triangle - (25.3939245, -72.473816)
-		console.log(bounds.getCenter());
+		if (countLat != 0 && countLng != 0){
+			centerLat = sommeLat / countLat;
+			centerLng = sommeLng / countLng;
+		}
 	}
 	
 
 	public static void main(String[] args) throws Exception {
-		// Interface fenetre = new Interface();
+		 //Interface fenetre = new Interface();
 
 		// Proxy du PUIO
 		System.setProperty("https.proxyHost", "cache.u-psud.fr");
 		System.setProperty("https.proxyPort", "8080");
 
-		/**
-		 * String addr = "213 rue de Versailles, 92410, Ville-d'Avray";
-		 * addrToCoord(addr, 3);
-		 * 
-		 * for(int i=0; i<coordLat.length; i++){ System.out.println("Lat : " +
-		 * coordLat[i]); System.out.println("Lng : " + coordLng[i]); }
-		 **/
+		
+		  String addr = "213 rue de Versailles, 92410, Ville-d'Avray";
+		  addrToCoord(addr, 3);
+		  
+		  for(int i=0; i<coordLat.length; i++){ System.out.println("Lat : " +
+		  coordLat[i]); System.out.println("Lng : " + coordLng[i]); }
+		 
 	}
 
 }
